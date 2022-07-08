191b4d665f3ca54369c1c06998e75ded83392af7
Suze Bird <suzembird@gmail.com>
2022-07-08 12:41:19 -0700

    some function written

--- index.js
+++ index.js
@@ -47,3 +47,27 @@ function every(arr, predicate) {
   }
 }
 
+const variable = every(myArray, (x) => x % 2 === 0);
+const variable2 = every([4, 8, 12, 14], (x) => x % 2 === 0);
+console.log(variable2);
+console.log(variable);
+
+//some
+
+function some(arr, predicate) {
+    for(let i = 0; i < arr.length; i++) {
+        const item = arr[i];
+        newItem = predicate(item);
+        if (newItem) {
+            return true;
+        } else { 
+            return 'false';
+        }
+    }
+}
+
+const someTest = some([1, 6, 5], (n) => n % 2 === 0);
+const someTest2 = some([1, 7, 3], (n) => n % 2 === 0);
+console.log("someTest", someTest);
+console.log("someTest2", someTest2);
+
